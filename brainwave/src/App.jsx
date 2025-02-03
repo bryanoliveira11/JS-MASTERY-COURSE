209a061ca5d39386c25be5a2a0bc51a1c15d1892
bryan <bryan.oliveira12345@gmail.com>
2025-02-03 11:16:43 -0300

    Button

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,7 +1,18 @@
-import "./index.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./Components/Button";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hey you</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hey you</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
