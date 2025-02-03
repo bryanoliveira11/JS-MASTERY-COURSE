b355d75266b4010417508a7dade3ea1ac70448e3
bryan <bryan.oliveira12345@gmail.com>
2025-02-03 10:33:37 -0300

    Tailwind setup and assets

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
+import "./index.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hey you</h1>
+  return <h1 className="text-3xl font-bold underline">Hey you</h1>;
 }
 
-export default App
+export default App;
