bb83df5f674c7534869cf2eb88d06ace6e9d7ca2
bryan <bryan.oliveira12345@gmail.com>
2025-02-06 10:34:09 -0300

    Header and constants

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,14 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./Components/Button";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hey you</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
