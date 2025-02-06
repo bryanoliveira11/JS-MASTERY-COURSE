67382ba98964396a221a47a18bc64db68aa0c1a4
bryan <bryan.oliveira12345@gmail.com>
2025-02-06 11:15:08 -0300

    Header end, Hamburger menu start

--- brainwave/src/Components/Header.jsx
+++ brainwave/src/Components/Header.jsx
@@ -1,12 +1,25 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
+  const pathname = useLocation();
+  const [openNavigation, setopenNavigation] = useState(true);
+
   return (
     <div
-      className="fixed top-0 z-50 bg-[var(--n-8)]/90
-    backdrop-blur-sm border-b border-[var(--n-6)] lg:bg-[var(--n-8)]/90
-    lg:backdrop-blur-sm"
+      className={`fixed top-0 left-0 w-full z-50 bg-[var(--n-8)]/90
+    backdrop-blur-sm border-b border-[var(--n-6)] 
+    lg:bg-[var(--n-8)]/90 lg:backdrop-blur-sm
+     ${
+       openNavigation
+         ? "bg-[var(--n-8)]"
+         : "bg-[var(--n-8)]/90 backdrop-blur-sm"
+     }`}
     >
       <div
         className="flex items-center px-5 lg:px-7.5
@@ -17,9 +30,9 @@ const Header = () => {
         </a>
 
         <nav
-          className="hidden fixed top-[5rem] 
+          className={`${openNavigation ? "flex" : "hidden"} fixed top-[5rem] 
         left-0 right-0 bg-[var(--n-8)] lg:static
-         lg:flex lg:mx-auto lg:bg-transparent"
+         lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div
             className="relative z-2 flex flex-col 
@@ -34,13 +47,32 @@ const Header = () => {
                 hover:text-[var(--color-1)]
                 ${item.onlyMobile ? "lg:hidden" : ""}
                 px-6 py-6 md:py-8 lg:-mr-0.5 lg:text-xs
-                lg:font-semibold`}
+                lg:font-semibold
+                ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-[var(--n-1)]"
+                    : "lg:text-[var(--n-1)]/50"
+                } lg:leading-5 lg:hover:text-[var(--n-1)]
+                xl:px-12`}
               >
                 {item.title}
               </a>
             ))}
+            <HamburgerMenu />
           </div>
         </nav>
+        <div className="hidden lg:flex items-center space-x-4">
+          <a
+            href="#signup"
+            className="button text-[12px] text-[var(--n-1)]/50 
+      transition-colors hover:text-[var(--n-1)] uppercase"
+          >
+            New Account
+          </a>
+          <Button className="lg:flex" href="#login">
+            Sign In
+          </Button>
+        </div>
       </div>
     </div>
   );
